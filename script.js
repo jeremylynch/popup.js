096fe2e85fd06f13932ae9ea1fe46b7b245f7a84
David Roussov <david.roussov@gmail.com>
2019-12-20 18:32:05 +1100

    show popup is global is accepts html string

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
   (function() {
     setTimeout(() => {
 
-      var isUK = true;
-      var auLink = '#';
-      var ukLink = '#';
-      var modalContainerId = uuidv4();
-      var cname = 'locale-modal';
+      var popupContainerId = uuidv4();
+      var cname = 'locale-popup';
 
-      function showModal() {
-        var modalContainer = document.createElement('div');
-        modalContainer.id = modalContainerId;
-        modalContainer.innerHTML = "<div style='all: initial'><div style='position: fixed; top: 0; right: 0; left: 0; bottom: 0; z-index: 1060; opacity: 0.7; background-color: #6c757d;'></div><div style='z-index: 1070; font-family: Arial; background: white; border-radius: 5px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 50px; color: gray;'><h2>Are you visiting from outside Australia? Visit the regional website for more relevant content.</h2><div style='width: 100%; margin-bottom: 20px; text-align: center; font-size: 1.5em;'><a href=\"".concat(ukLink, "\">Continue to UK</a></div>\n<div style='width: 100%; margin-bottom: 20px; text-align: center; font-size: 1.5em;'><a href=\"").concat(auLink, "\">Continue to Australia</a></div></div></div>");
+      function showPopup(html) {
+
+        var cookie = getCookie(cname);
+
+        // if true, region locale popup has already been seen, so we do not proceed
+        if (getCookie(cname) === 'true') {
+          return
+        }
+
+        var popupContainer = document.createElement('div');
+        popupContainer.id = popupContainerId;
+        popupContainer.innerHTML = html;
 
         var body = document.getElementsByTagName('body')[0];
-        body.appendChild(modalContainer);
+        body.appendChild(popupContainer);
+
+        setCookie(cname, true);
       }
 
-      function hideModal() {
-        const modalContainer = document.getElementById(modalContainerId);
-        while (modalContainer && modalContainer.firstChild) {
-          modalContainer.removeChild(modalContainer.firstChild);
+      function hidePopup() {
+        const popupContainer = document.getElementById(popupContainerId);
+        while (popupContainer && popupContainer.firstChild) {
+          popupContainer.removeChild(popupContainer.firstChild);
         }
       }
 
@@ -53,17 +60,6 @@
         return "";
       }
 
-      var cookie = getCookie(cname);
-
-      // if true, region locale modal has already been seen, so we do not proceed
-      if (getCookie(cname) === 'true') {
-        return
-      }
-
-      // TODO: check user locale
-      // if region not aus, then set cookie and show modal
-      // setCookie(cname, true);
-      showModal();
-
+      window.showPopup = showPopup;
     });
   })()
