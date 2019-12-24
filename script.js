6031095d7b4de2428ee32f60e76051fd47bdc891
Jeremy Lynch <jeremylynch1994@gmail.com>
2019-12-24 15:48:51 +1000

    use session storage

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 var popupContainerId = uuidv4();
-var cname = 'locale-popup';
 
 function showPopup(popupBodyHtml) {
 
-  var cookie = getCookie(cname);
-
-  // if true, region locale popup has already been seen, so we do not proceed
-  if (getCookie(cname) === 'true') {
+  console.log('hi')
+  if (window.sessionStorage.popupShown === "true") {
     return
   }
 
@@ -74,7 +71,7 @@ function showPopup(popupBodyHtml) {
   var body = document.getElementsByTagName('body')[0];
   body.appendChild(popupContainer);
 
-  setCookie(cname, true);
+  sessionStorage.popupShown = true
 
   document.addEventListener('click', function(e) {
     if (!popup.contains(e.target)) {
@@ -95,29 +92,6 @@ function uuidv4() {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
-}
-
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
 }
 
 window.showPopup = showPopup;
